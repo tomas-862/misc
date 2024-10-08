6ee47e315ec9fc31b7a927401ce00dfe0a588d8b
tomas-862 <tomas.petraitis@icloud.com>
2024-10-08 17:21:02 +0300

    adding hello py

---
+++ cs50/hello.py
@@ -0,0 +1,5 @@
+# In a file called hello.py in a folder called sentimental-hello, implement a program that prompts a user for their name, 
+# and then prints hello, so-and-so, where so-and-so is their provided name.
+
+name = input("Whats is your name? ")
+print("hello,",name)
