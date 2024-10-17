5e9360724c462d1ac538f31dc82d28e5880cad32
tomas-862 <tomas.petraitis@icloud.com>
2024-10-17 16:48:35 +0300

    lab2 py

---
+++ spcore/Lab2/lab2.py
@@ -0,0 +1,15 @@
+# create file lab2.py
+# file must print following: 
+# show ip interface brief
+# show ip route | include 192.168.1.0
+# show interface status
+# This lab is Tomas CCNP SP lab
+# The password is "Cisco"
+
+
+
+print("show ip interface brief")
+print("show ip route | include 192.168.1.0")
+print("show interface status")
+print("This lab is Tomas CCNP SP lab")
+print('The password is "Cisco"')
