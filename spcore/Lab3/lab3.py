efb520f5c4e465a3cf568d63e5dd8609b0498707
tomas-862 <tomas.petraitis@icloud.com>
2024-10-17 17:07:57 +0300

    adding lab3 py file

---
+++ spcore/Lab3/lab3.py
@@ -0,0 +1,24 @@
+# Lab with simple math
+
+print("How much is 3 + 2")
+print(3 + 2)
+print() # This prints an empty line
+print("if 1 Dollar is 2 Framnks, then hwo much is 10 Dollar in Franks")
+print( 10 * 2)
+print() # This prints an empty line
+print("what is 3 + 2 + 5 + -2 * 4")
+print (3 + 2 + 5 + -2 * 4)
+print() # This prints an empty line
+print("what is 4 + 2 + 5 * 2 / 4 + 2")
+print(4 + 2 + 5 * 2 / 4 + 2)
+print() # This prints an empty line
+print("is 3 + 2 =5")
+print(3 +2 == 5)
+print() # This prints an empty line
+
+print() # This prints an empty line
+
+print() # This prints an empty line
+
+print() # This prints an empty line
+
