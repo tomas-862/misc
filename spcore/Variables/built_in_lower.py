1650536509c4f7d6f4a1222ef0482e9cd021ae64
tomas-862 <tomas.petraitis@icloud.com>
2024-10-21 16:54:08 +0300

    built in files

---
+++ spcore/Variables/built_in_lower.py
@@ -0,0 +1,8 @@
+# we will use built-in 'lower' to convert and print text in lower case. 
+text = input("Enter text: ")
+
+# we will print text in lower case 
+print(text.lower())
+
+
+
---
+++ spcore/Variables/built_in_startswith.py
@@ -0,0 +1,8 @@
+# We will use buil-in libraries 
+
+################# 'startwith' #############
+# Set variable
+IP_Address = "192.168.1.1"
+
+# Check if variable IP_Address starts with "192" and print boolean (True or False)
+print(IP_Address.startswith("192"))
