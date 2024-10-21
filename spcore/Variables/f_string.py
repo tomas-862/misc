03e962cfa6483cdba35a366470b1cb0dd4542439
tomas-862 <tomas.petraitis@icloud.com>
2024-10-21 09:52:44 +0300

    renamed some items

---
+++ spcore/Variables/f_string.py
@@ -0,0 +1,12 @@
+# variables
+First_Name = "Tomas"
+Last_Name = "Petr" 
+Height = "190 cm."
+Weight = "100 kg."
+Eyes = "Blue"
+
+# will create formatted string using f-string and concatenate my variables
+Full_Name = f"My full name is {First_Name} {Last_Name} and my height is {Height}"
+
+print(Full_Name)
+
