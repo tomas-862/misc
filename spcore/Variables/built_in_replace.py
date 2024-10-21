ae055d6585c591544fbfdb3eaa6632330cce7346
tomas-862 <tomas.petraitis@icloud.com>
2024-10-21 17:41:32 +0300

    changes

---
+++ spcore/Variables/built_in_replace.py
@@ -0,0 +1,11 @@
+## we will use built-in 'replace' to replace '.' to ':' in mac address
+mac_address = input("Enter mac address in format aa.bb.cc.dd.ee.ff: ")
+
+# using 'replace' we will print out mac address in format aa:bb:....
+# built in 'replace' usage is VAR.replace("A" , "C")
+# where VAR - variable where to look at. 
+# where A - what to replace
+# where C - to what to replace
+print("mac_address in new format (mac_address.replace("." , ":")"))
+
+
