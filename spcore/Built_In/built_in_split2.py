ec9ff20a1f97cc1c7f1f4ec6b7698d685f12d8ff
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-10-22 21:58:14 +0300

    Update built_in_split2.py

--- spcore/Built_In/built_in_split2.py
+++ spcore/Built_In/built_in_split2.py
@@ -17,3 +17,24 @@ updated_show_run = show_run.split("\n")
 print(show_run.splitlines())
 print()
 
+
+
+# This script demonstrates the use of the escape sequence '\n'.
+# '\n' is a special escape sequence that represents a newline character. 
+# When included in a string, it instructs Python to start a new line wherever it appears.
+
+show_run = "interface G0/0/0\nno shut\ndescription Connected to CSR1\nip address 10.0.0.1 255.255.255.0"
+print()
+print(show_run)
+print()
+
+# We will 'split' our script using '\n' as the delimiter.
+updated_show_run = show_run.split("\n")
+print(updated_show_run)
+print()
+
+# We can achieve the same result using the 'splitlines' built-in method without specifying '\n' as the delimiter.
+updated_show_run = show_run.splitlines()
+print(updated_show_run)
+print()
+
