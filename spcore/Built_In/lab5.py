2db1fd9d684fb2b1bceb0d5b8ae8222b2cb459b7
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-10-22 21:59:47 +0300

    Update lab5.py

--- spcore/Built_In/lab5.py
+++ spcore/Built_In/lab5.py
@@ -1,30 +1,29 @@
-# create a var string "ccie" with value "CCIE Enterprise Infrasttucture\n CCIE Service Provider\n CCIE Security"
-ccie = "CCIE Enterprise Infrasttucture\n CCIE Service Provider\n CCIE Security"
+# Create a variable 'ccie' with a multiline string containing different CCIE tracks.
+ccie = "CCIE Enterprise Infrastructure\nCCIE Service Provider\nCCIE Security"
 
-# create a var string "ip_address1" value "10.1.2.100"
+# Create a variable 'ip_address1' with the value "10.1.2.100"
 ip_address1 = "10.1.2.100"
 
-# create a var string "ip_address2" value "10.3.4.200"
+# Create a variable 'ip_address2' with the value "10.3.4.200"
 ip_address2 = "10.3.4.200"
 
-# store  var "ccie" in the new var called "lower_ccie". Value must be in lower case.
+# Convert the content of 'ccie' to lowercase and store it in a new variable called 'lower_ccie'.
 lower_ccie = ccie.lower()
 
-# split content of ip_address1 to the list called "new_ip_address1"
+# Split the content of 'ip_address1' into a list called 'new_ip_address1'.
 new_ip_address1 = ip_address1.split(".")
 
-# split content of ip_address2 to the list called "new_ip_address2"
+# Split the content of 'ip_address2' into a list called 'new_ip_address2'.
 new_ip_address2 = ip_address2.split(".")
 
-# split contect of "lower_ccie" into the list "new_lower_ccie"
+# Split the content of 'lower_ccie' into a list called 'new_lower_ccie'.
 new_lower_ccie = lower_ccie.split()
 
-# print 3rd octet of "new_ip_address1"
-print(new_ip_address1[2])
+# Print the 3rd octet of 'new_ip_address1'.
+print(new_ip_address1[2])  # Output: 2
 
-# print 4th octet of "new_ip_address2"
-print(new_ip_address2[3])
+# Print the 4th octet of 'new_ip_address2'. Note that list indexing starts at 0.
+print(new_ip_address2[2])  # Output: 200
 
-# print "new_lower_ccie"
+# Print the list 'new_lower_ccie'.
 print(new_lower_ccie)
-
