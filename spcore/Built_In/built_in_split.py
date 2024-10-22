eb7186dfb90b8dfee772636d14bc5edf9851dc5c
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-10-22 21:55:29 +0300

    Update built_in_split.py

--- spcore/Built_In/built_in_split.py
+++ spcore/Built_In/built_in_split.py
@@ -1,16 +1,17 @@
-# we will use 'split' bult-in  to split variable
+# We will use the built-in 'split' method to split a variable.
 
-# set variable 
+# Set the variable for the IP address
 IP_Address = "192.168.1.1"
 
-# we will use 'split', in the bracket we define where we want to split. We will at "."
-# we will print and will verify type of split variable
-print(type(IP_Address))
-print(IP_Address.split("."))
-print(type(IP_Address.split(".")))
+# We will use 'split' to divide the string at each occurrence of the "." character.
+# We will print the original type of the variable and the result of the split operation.
+print(type(IP_Address))  # Output the type of the original variable
+print(IP_Address.split("."))  # Print the result of splitting the IP address
+print(type(IP_Address.split(".")))  # Output the type of the result after splitting
 print()
-# we can see that split variable is list, not string anymore
 
-# define new variable 'IP_List' and print 3rd list item. 
+# We can see that the result of the split operation is a list, not a string anymore.
+
+# Define a new variable 'IP_List' and print the 4th item (index 3) from the list.
 IP_List = IP_Address.split(".")
-print(IP_List[3])
+print(IP_List[3])  # Output the 4th item in the list
