2679f09e8189f5be33e54cef1b89ebdb8a7f5cf9
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-10-22 21:53:10 +0300

    Update built_in_format.py

--- spcore/Built_In/built_in_format.py
+++ spcore/Built_In/built_in_format.py
@@ -1,9 +1,9 @@
-# create variables
+# Create variables for CCIE certification and the number of certified individuals
 ccie2 = "CCIE Security"
-num_of_sec = input("How many CCIE Sec certified people: ")
+num_of_sec = input("How many CCIE Security certified people are there? ")
 
-# create variable 'total_sec' with 2x {} in which I can specify variables
-total_sec = "There are {} {} certified people in the world\n"
+# Create a variable 'total_sec' that includes placeholders for formatting
+total_sec = "There are {} {} certified people in the world.\n"
 
-# will use buil-in 'format' and insert variables to hte {} of the variable 'total_sec'
-print(total_sec.format (num_of_sec, ccie2))
+# Use the built-in 'format' method to insert variables into the placeholders of 'total_sec'
+print(total_sec.format(num_of_sec, ccie2))
