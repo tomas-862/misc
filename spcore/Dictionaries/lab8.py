efb447f3fa440941c66df4ecd6e597eeb34e7696
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-10-23 16:30:49 +0000

    lab 8

--- spcore/Dictionaries/lab8.py
+++ spcore/Dictionaries/lab8.py
@@ -1,1 +1,23 @@
-li
+# lab8.py
+
+# Define a dictionary "info1" with key-value pairs
+info1 = {
+    "hostname": "R1",
+    "mgmt_ip": "10.1.1.1",
+    "username": "Tom",
+    "password": "cisco"
+}
+
+# Define a dictionary "info2" with key-value pairs
+info2 = {
+    "hostname": "R2",
+    "mgmt_ip": "10.1.1.2",
+    "username": "Tom",
+    "password": "cisco"
+}
+
+# Create a list named "info" that contains the dictionaries "info1" and "info2"
+info = [info1, info2]
+
+# Print the list "info"
+print(info)
