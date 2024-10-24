b8e0b695f5e0c4338128d46bf781e3ffc21eedc5
tomas-862 <tomas.petraitis@icloud.com>
2024-10-24 20:56:52 +0300

    update lab9

--- CCNP_SP_SPCORE/Nested_obj/la9.py
+++ CCNP_SP_SPCORE/Nested_obj/la9.py
@@ -1,64 +1,56 @@
-# Create var of class dictionary "dic1" with key and value set (hostname, mgmt-ip, username, password)
-# Create var of class dictionary "dic2" with key and value set (hostname, mgmt-ip, username, password)
-# Create var of class dictionary "interface1" with key and value set (interface, ip_address)
-# Create var of class dictionary "interface2" with key and value set (interface, ip_address)
-# Create var of class list "data_center" with index 0 set as "dict1" and index 1 set as "dict2".
-# Add / merge variable "interface1" to with "dict1" and "interface2" to with "dict2", so "interface1" will be nested to "dict1" and "interface2" with "dict2" Print it.
+# Import the JSON module for pretty printing
+import json
 
-
-## with JSON we can perform more pretty print out, to visualise hierarfy of nested objects. 
-## for this we will use json module in py and command "print(json.dump(data_center, ident=4))", 
-## where "data_center" our variable and and index 4 ditance form the margins. 
-
-
-# Import JSON and pretty print IP address of R1
-# Import JSON and pretty print "data_center"
-
-
-
-# Create var of class dictionary "dic1" with key and value set (hostname, mgmt-ip, username, password)
-dict1 = {
+# Create a dictionary variable "dic1" with key-value pairs: hostname, mgmt-ip, username, password
+dic1 = {
     "hostname": "R1",
     "mgmt-ip": "10.1.1.1",
     "username": "tom",
     "password": "cisco"
 }
 
-# Create var of class dictionary "dic2" with key and value set (hostname, mgmt-ip, username, password)
-dict2 = {
+# Create a dictionary variable "dic2" with key-value pairs: hostname, mgmt-ip, username, password
+dic2 = {
     "hostname": "R2",
     "mgmt-ip": "10.1.1.2",
     "username": "tom",
     "password": "cisco"
 }
 
-# Create var of class dictionary "interface1" with key and value set (interface, ip_address)
-Interface1 = {
+# Create a dictionary variable "interface1" with key-value pairs: interface and ip_address
+interface1 = {
     "interface": "G1",
     "ip_address": "192.168.1.1"
 }
 
-# Create var of class dictionary "interface2" with key and value set (interface, ip_address)
-Interface2 = {
+# Create a dictionary variable "interface2" with key-value pairs: interface and ip_address
+interface2 = {
     "interface": "G2",
     "ip_address": "192.168.1.2"
 }
 
-# Create var of class list "data_center" with index 0 set as "dict1" and index 1 set as "dict2". Print.
-data_center = [dict1, dict2]
-print(data_center)
+# Create a list variable "data_center" with index 0 set to "dic1" and index 1 set to "dic2"
+data_center = [dic1, dic2]
+
+# Print the initial data_center list
+print("Initial data_center:")
+print(json.dumps(data_center, indent=4))  # Using JSON printing for readability
 print()
 
-# Add / merge variable "interface1" to with "dict1" and "interface2" to with "dict2", so "interface1" will be nested to "dict1" and "interface2" with "dict2". Print. 
-data_center [0]["interface"] = Interface1
-data_center [1]["interface"] = Interface2
-print(data_center)
+# Nest "interface1" into "dic1" and "interface2" into "dic2"
+data_center[0]["interface"] = interface1
+data_center[1]["interface"] = interface2
+
+# Print the updated data_center list with nested interfaces
+print("Updated data_center with nested interfaces:")
+print(json.dumps(data_center, indent=4))  # Pretty print the updated structure
 print()
 
-# Import JSON and pretty print IP address of R1
-import json
-print(json.dumps(Interface1, indent=4))
+# Pretty print the details of "interface1"
+print("Pretty printed interface1:")
+print(json.dumps(interface1, indent=4))  # Displays interface1 in a readable format
+print()
 
-# Pretty print "data_center"
-print(json.dumps(data_center, indent=4))
-
+# Pretty print the entire "data_center" for a clear view of the hierarchy
+print("Pretty printed data_center:")
+print(json.dumps(data_center, indent=4))  # Visualizes the full structure of data_center
