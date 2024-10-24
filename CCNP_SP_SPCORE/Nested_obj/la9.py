beec64b35a819de0f49a29c15563b6c60e9d57db
tomas-862 <tomas.petraitis@icloud.com>
2024-10-24 20:43:25 +0300

    update lab9

--- CCNP_SP_SPCORE/Nested_obj/la9.py
+++ CCNP_SP_SPCORE/Nested_obj/la9.py
@@ -3,7 +3,14 @@
 # Create var of class dictionary "interface1" with key and value set (interface, ip_address)
 # Create var of class dictionary "interface2" with key and value set (interface, ip_address)
 # Create var of class list "data_center" with index 0 set as "dict1" and index 1 set as "dict2".
-# Add / merge variable "interface1" to with "dict1" and "interface2" to with "dict2", so "interface1" will be nested to "dict1" and "interface2" with "dict2" Print it. 
+# Add / merge variable "interface1" to with "dict1" and "interface2" to with "dict2", so "interface1" will be nested to "dict1" and "interface2" with "dict2" Print it.
+
+
+## with JSON we can perform more pretty print out, to visualise hierarfy of nested objects. 
+## for this we will use json module in py and command "print(json.dump(data_center, ident=4))", 
+## where "data_center" our variable and and index 4 ditance form the margins. 
+
+
 # Import JSON and pretty print IP address of R1
 # Import JSON and pretty print "data_center"
 
@@ -48,4 +55,10 @@ data_center [1]["interface"] = Interface2
 print(data_center)
 print()
 
+# Import JSON and pretty print IP address of R1
+import json
+print(json.dumps(Interface1, indent=4))
+
+# Pretty print "data_center"
+print(json.dumps(data_center, indent=4))
 
