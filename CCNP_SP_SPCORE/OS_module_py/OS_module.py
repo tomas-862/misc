87e8000e52ea9eece945327ef0343cb3a348261e
tomas-862 <tomas.petraitis@icloud.com>
2024-10-25 15:21:47 +0300

    update

--- CCNP_SP_SPCORE/OS_module_py/OS_module.py
+++ CCNP_SP_SPCORE/OS_module_py/OS_module.py
@@ -7,12 +7,12 @@ print(os.getcwd())
 print()
 
 # change dir to "Git" and print. 
-os.chdir("/mnt/c/Users/tomas/OneDrive/Documents/Git")
+os.chdir("C:/Users/tomas/OneDrive/Documents/Git")
 print (os.getcwd())
 
 # navigate to direcotry "OS_module_py", create file "test.txt", print out list of all the files in the dirdorectory to ensire test.txt is here.
-os.chdir("/mnt/c/Users/tomas/OneDrive/Documents/Git/misc/CCNP_SP_SPCORE/OS_module_py")
-os.mkdir ("test.md")
+os.chdir("C:/Users/tomas/OneDrive/Documents/Git/misc/CCNP_SP_SPCORE/OS_module_py")
+os.mkdir ("test.txt")
 print(os.listdir())
 
 
