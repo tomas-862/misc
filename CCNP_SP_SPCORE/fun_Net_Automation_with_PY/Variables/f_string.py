06bb7565a7afa07437555bff703b1a641a2c3451
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-11-14 23:11:13 +0200

    Update f_string.py

--- CCNP_SP_SPCORE/fun_Net_Automation_with_PY/Variables/f_string.py
+++ CCNP_SP_SPCORE/fun_Net_Automation_with_PY/Variables/f_string.py
@@ -3,7 +3,7 @@ first_name = "Tomas"
 last_name = "Petr"
 height = "1900 cm"
 weight = "1000 kg"
-eyes = "Blue"
+eyes = "Blue Sky"
 
 # Create a formatted string using f-string to concatenate variables
 full_name = f"My full name is {first_name} {last_name}, my height is {height}, my weight is {weight}, and my eye color is {eyes}."
