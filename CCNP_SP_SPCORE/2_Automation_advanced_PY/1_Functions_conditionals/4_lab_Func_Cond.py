43a77c38bbce5a02260346a4431eb689ce277151
tomas-862 <168358137+tomas-862@users.noreply.github.com>
2024-11-19 21:25:36 +0200

    Update 4_lab_Func_Cond.py

--- CCNP_SP_SPCORE/2_Automation_advanced_PY/1_Functions_conditionals/4_lab_Func_Cond.py
+++ CCNP_SP_SPCORE/2_Automation_advanced_PY/1_Functions_conditionals/4_lab_Func_Cond.py
@@ -1,20 +1,20 @@
-#1 Create a variable of class dict named "dict1" which contains a key and value set as:
+# 1 Create a variable of class dict named "dict1" which contains a key and value set as:
 ## {"hostname": "R1", "OS": "IOS-XE", "mgmt-ip": "10.1.1.1"}
 
-#2 Create a variable of class dict named "dict2" which contains a key and value set as:
+# 2 Create a variable of class dict named "dict2" which contains a key and value set as:
 ## {"hostname": "R2", "OS": "IOS-XR", "mgmt-ip": "10.1.1.2"}
 
-#3 Create a function named "device_ip" to display the following management IP of the device. 
+# 3 Create a function named "device_ip" to display the following management IP of the device. 
 ## The management IP must be displayed based on the following conditions:
 ### If OS is IOS-XE, then print "The management IP of {hostname} is {mgmt-ip}."
 ### If OS is IOS-XR, then print "The management IP of {hostname} is {mgmt-ip}."
 ### If OS is NEXUS, then print "This device has an unknown image."
 
-#4 Save the function as Lab_Func_Cond.py
+# 4 Save the function as Lab_Func_Cond.py
 
-#5 Ensure you are able to call this function by importing it using the command "from Lab_Func_Cond import device_ip."
+# 5 Ensure you are able to call this function by importing it using the command "from Lab_Func_Cond import device_ip."
 
-#1 and 2 
+# 1 and 2 
 def device_ip(temp):
        dict1 = {"hostname": "R1", "OS": "IOS-XE", "mgmt-ip": "10.1.1.1"}
        dict2 = {"hostname": "R2", "OS": "IOS-XR", "mgmt-ip": "10.1.1.2"}
@@ -25,7 +25,7 @@ def device_ip(temp):
        else:
         print("This device has a unknown image")
     
-#5 
+# 5 
 
 # we go to Python, and from here import funcion from our file
 ## >>> from Lab_Func_Cond import device_ip
